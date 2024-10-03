8a683a87fb63903cdfec1635d998258ece944556
J Cui <i@cjxol.com>
2024-10-03 16:35:47 +0100

    Add user login

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string) {
+    const user = await this.userService.findOne(email);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user) {
+    const payload = {
+      email: user.email,
+      name: user.name,
+      superuser: user.superuser,
+    }
+    return {
+      access_token: this.jwtService.sign(payload),
+    }
+  }
+}
